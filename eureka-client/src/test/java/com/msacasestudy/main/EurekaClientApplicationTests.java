26ce864862a4e231a6373e94d31725b43146f007
Matt-Littlefield <matt.littlefield@temple.edu>
2025-04-10 18:19:42 -0400

    initialize the project

---
+++ eureka-client/src/test/java/com/msacasestudy/main/EurekaClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msacasestudy.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
